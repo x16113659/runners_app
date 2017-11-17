9e422bff343c2de5afde5c98d32428ae03a70974
Diana Viktor <x16113659@student.ncirl.ie>
2017-11-17 22:10:02 +0000

    static html pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
