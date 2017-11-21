9c0ade26cd7004a00e2d9825450f59e0ae98e18e
Diana Viktor <x16113659@student.ncirl.ie>
2017-11-21 23:02:16 +0000

    Added user and cart controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :items
+  get 'cart/index'
+  get 'site/about'
+  
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/admin' => 'user#admin_login'
+  get '/logout' => 'user#logout'
+
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clear'
+  get '/cart/:id' => 'cart#add'
+
   root 'static_pages#home'
 
-  get '/help', to: 'static_pages#help'
-
-  get '/about', to: 'static_pages#about'
+  resources :categories
+  resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
