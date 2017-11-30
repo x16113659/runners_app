5b2a06055bf2300d79cc880d61131817d7b4d233
Diana Viktor <x16113659@student.ncirl.ie>
2017-11-30 11:26:07 +0000

    devise views

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'cart/index'
-  get 'site/about'
-  
+
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
 
   get '/categories/list/:id' => 'categories#listitems'
 
